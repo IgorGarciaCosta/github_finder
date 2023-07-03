352c6a4e83f7d158bbaeed11bd3b763aa79f451f
ISILV125 <isilv125@ford.com>
2023-07-03 10:53:05 -0300

    getting specific data from json file

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,6 @@
 //rafce
 import React, { useState } from "react";
-import Search from "../components/search";
+import Search from "../components/Search";
 import { UserProps } from "../types/users";
 
 const Home = () => {
@@ -8,14 +8,23 @@ const Home = () => {
 
   const loadUser = async (userName: string) => {
     const res = await fetch(`https://api.github.com/users/${userName}`);
-    const data = res.json();
+    const data = await res.json();
 
-    console.log(data);
+    const { avatar_url, login, location, followers, following } = data;
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+    setUser(userData);
   };
 
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}
     </div>
   );
 };
