b2168f50ab3ef341a633abe36d086337a3e2a653
ISILV125 <isilv125@ford.com>
2023-07-03 12:54:35 -0300

    Finalized css part of the user card

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { UserProps } from "../types/Users";
 import { MdLocationPin } from "react-icons/md";
 import { Link } from "react-router-dom";
+import classes from "./User.module.css";
 
 const User = ({
   avatar_url,
@@ -11,24 +12,24 @@ const User = ({
   following,
 }: UserProps) => {
   return (
-    <div>
+    <div className={classes.user}>
       <img src={avatar_url} alt={login} />
       <h2>{login}</h2>
       {location && (
-        <p>
+        <p className={classes.location}>
           <MdLocationPin />
           <span>{location}</span>
         </p>
       )}
 
-      <div>
+      <div className={classes.stats}>
         <div>
           <p>Fellowers:</p>
-          <p>{followers}</p>
+          <p className={classes.number}>{followers}</p>
         </div>
         <div>
           <p>Following:</p>
-          <p>{following}</p>
+          <p className={classes.number}>{following}</p>
         </div>
       </div>
       <Link to="/">Show best projects</Link>
